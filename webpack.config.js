e1ad718a2b04e2b8301def82ea8181eaf4106d37
Kevinjovel <kevinjovel9@gmail.com>
2020-12-03 09:16:05 -0600

    Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
-const HtmlWebPackPlugin= require('html-webpack-plugin')
+const HtmlWebPackPlugin= require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports={
     entry: './src/index.js',
     output: {
@@ -25,14 +27,27 @@ module.exports={
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
-    },
-    plugins:[
+    }, 
+     plugins: [ 
         new HtmlWebPackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-
+            template: 'public/index.html',
+            filename: 'index.html'
         }),
+
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
     ]
 };
