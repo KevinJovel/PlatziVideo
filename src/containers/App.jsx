0d88880d883326c9a42d77534670e68d4d51fbe0
Kevin Jovel <kevinjovel9@gmail.com>
2020-12-07 16:03:02 -0600

    header and search container initial commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import '../assets/styles/App.scss';
+const APP =()=>(
+    <div className="App">
+        <Header/>
+        <Search/>
+    </div>
+);
+export default APP;
