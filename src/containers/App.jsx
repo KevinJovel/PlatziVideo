210525deb2e115c3dad6e394b4dccf609f13dced
Kevin Jovel <kevinjovel9@gmail.com>
2020-12-13 17:55:15 -0600

    funcionando

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
-const APP =()=>(
-    <div className="App">
-        <Header/>
-        <Search/>
+
+const API = 'http://localhost:3000/initalState';
+const APP = () => {
+  const initialState = useInitialState(API);
+  // console.log(videos);
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title='My list'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Tendencias'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
+        </Carousel>
+      </Categories>
+      <Footer />
     </div>
-);
+  );
+};
 export default APP;
