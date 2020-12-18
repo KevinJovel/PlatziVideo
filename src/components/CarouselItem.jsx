7d7e8c300acd822f8dc3dce40d654455202b5efa
Kevin Jovel <kevinjovel9@gmail.com>
2020-12-18 14:13:35 -0600

    remove item

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import '../assets/styles/components/CarouselItem.scss';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setFavorite } from '../actions';
+import { setFavorite, deleteFavorite } from '../actions';
 import playIcon from '../assets/static/play-icon.png';
 import plusicon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove.png';
 
 const CarouselItem = (props) => {
   const { id, cover, title, year, duration, contentRating } = props;
@@ -18,6 +19,9 @@ const CarouselItem = (props) => {
       contentRating,
     });
   };
+  const handleDeleteFavorite = (itemId) => (
+    props.deleteFavorite(itemId)
+  );
   return (
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
@@ -25,6 +29,7 @@ const CarouselItem = (props) => {
         <div>
           <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
           <img className='carousel-item__details--img' src={plusicon} alt='Plus Icon' onClick={handleSetFavorite} />
+          <img className='carousel-item__details--img' src={removeIcon} alt='Remove Icon' onClick={() => handleDeleteFavorite(id)} />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
         <p className='carousel-item__details--subtitle'>
@@ -44,6 +49,7 @@ CarouselItem.propTypes = {
 };
 const mapDispatchToProps = {
   setFavorite,
+  deleteFavorite,
 };
 // export default CarouselItem;
 export default connect(null, mapDispatchToProps)(CarouselItem);
