bc343b17e4dbd4cccebcc299ded6b4a906e0c25e
Kevin Jovel <kevinjovel9@gmail.com>
2021-01-24 12:13:30 -0600

    server side render config

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers';
+import initalState from '../frontend/initalState';
 
 dotenv.config();
 
@@ -21,25 +30,38 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 
 }
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html) => {
+  return (`
   <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/app.css" type="text/css">
-    <title>React App Plazi Video</title>
-</head>
-<body>
-    <div id="app"></div>
-    <script src="assets/app.js" type="text/javascript"></script>
-</body>
-</html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="assets/app.css" type="text/css">
+      <title>React App Plazi Video</title>
+  </head> 
+  <body>
+      <div id="app">${html}</div>
+      <script src="assets/app.js" type="text/javascript"></script>
+  </body>
+  </html>
   `);
-});
+};
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initalState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+  res.send(setResponse(html));
+};
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
   else console.log('Server running on port 3000');
 });
+ 
